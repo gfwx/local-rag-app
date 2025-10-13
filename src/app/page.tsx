676c2db3616a4f8daf83e9a1afd4f71ad9517e61
gfwx <gfawkes1832@gmail.com>
2025-10-13 17:18:56 +0530

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,14 +29,20 @@ export default function Chat() {
   };
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div className="flex flex-col w-full max-w-4xl py-24 mx-auto stretch">
       {messages.map((message) => (
         <div key={message.id} className="whitespace-pre-wrap">
-          {message.role === "user" ? "User: " : "AI: "}
+          {message.role === "user"
+            ? "User: "
+            : message.role === "assistant"
+              ? "AI: "
+              : ""}
           {message.parts.map((part, i) => {
-            switch (part.type) {
-              case "text":
-                return <div key={`${message.id}-${i}`}>{part.text}</div>;
+            if (message.role === "user" || message.role === "assistant") {
+              switch (part.type) {
+                case "text":
+                  return <div key={`${message.id}-${i}`}>{part.text}</div>;
+              }
             }
           })}
         </div>
@@ -45,15 +51,13 @@ export default function Chat() {
       <form onSubmit={handleSubmit}>
         {status !== "ready" && <p color="blue">Loading AI Response</p>}
         <input
-          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          className="fixed dark:bg-zinc-900 max-w-4xl bottom-0 w-full p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
           disabled={status !== "ready"}
           onChange={(e) => setInput(e.currentTarget.value)}
         />
       </form>
-
-      <button onClick={() => console.log(messages)}> Click me </button>
     </div>
   );
 }
